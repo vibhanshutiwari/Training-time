0a18069355ce2ca13ce54048a50344c15f0f05d1
vibhanshutiwari <vibhanshutiwari1999@gmail.com>
2020-05-11 20:42:38 +0530

    Demo Data

--- Container/utility.js
+++ Container/utility.js
@@ -1,15 +1,34 @@
 
 const getData = (start = 10, end = 50) => {
-    let array = [];
-    for (let i = start; i <= end; i++) {
-      array.push(i);
-    }
-    return array;
+  let array = [];
+  for (let i = start; i <= end; i++) {
+    array.push(i);
+  }
+  return array;
 }
 
 
+const stringArray = () => {
+  let fruitlist =
+    [
+      'cherry', 'orange', 'apple'
+    ];
+  return fruitlist;
+}
 
 
-module.exports ={
-    getData :getData
+const falsyRemove = () => {
+   let falsyValue = 
+   [
+      1, '', undefined, 0, ``, false, 'shubham', NaN, 10
+   ];
+   return falsyValue ;
+}
+
+
+module.exports =
+{
+  getData: getData,
+  stringArray: stringArray,
+  falsyRemove: falsyRemove
 };
