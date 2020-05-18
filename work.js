8a6bde3d21bd539e1d36c20943e85ebe5f7ae9be
vibhanshutiwari <vibhanshutiwari1999@gmail.com>
2020-05-18 21:26:21 +0530

    chechobject

--- work.js
+++ work.js
@@ -1,12 +1,23 @@
 
-
-const data = 
-[
-1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+let arr = [
+    {
+        name: 'shubham',
+        place: 'delhi',
+        work: 'mumbai', 
+    }
 ];
 
- const value = data.filter(fun => {
-      return fun % 2 == 0;
-  });
-    console.log(`Even Number --  ${value}`);
+    
+    const display = () => {
+ 
+        arr.forEach(num => {
+    
+            console.log(num);
+
+        });
+        
+    }
+
+     display();
+
+    
