f28f97656dcf9abc67acc63dbf5b4fc3e8674c81
vibhanshutiwari <vibhanshutiwari1999@gmail.com>
2020-05-19 09:07:47 +0530

    object loop in array

---
+++ object/loopObject.js
@@ -0,0 +1,28 @@
+
+const list = [{
+    name: 'shubham',
+    id: 1,
+},
+{
+    name: 'manish',
+    id: 2,
+},
+{
+    name: 'shubham',
+    id: 1.
+}
+,
+{
+    name: 'shubham',
+    id: 1.
+}
+];
+
+const objectloop = () => {
+
+    for(let i =0; i < list.length; i++) {
+        console.log(list[i]);
+    }
+}
+
+    objectloop();
